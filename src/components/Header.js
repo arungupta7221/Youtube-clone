ee624ba181ee3ca52f4048ef1a7eea510141a87a
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-05-07 18:42:37 +0530

    added search functionility with youtube search api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import menu from '../assests/menu.png'
 import logo from '../assests/logo.png'
 import user from '../assests/user.png'
 import { useDispatch } from 'react-redux'
 import { toggleMenu } from '../utils/appSlice'
 import { Link } from 'react-router-dom'
+import { YOUTUBE_SEARCH_API } from '../utils/constant'
+// import { AiOutlineHistory } from 'react-icons/ai'
 
 const Header = () => {
+  const [search, setSearch] = useState('')
+  const [searchSuggestion, setSearchSuggestion] = useState([])
+  const [showSuggestion, setShowSuggestion] = useState(false)
   const dispatch = useDispatch()
   const handleToggle = () => {
     dispatch(toggleMenu())
+  }
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestion(), 200) //debouncing
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [search])
+  const getSearchSuggestion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + search)
+    const json = await data.json()
+    setSearchSuggestion(json[1])
   }
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
@@ -24,10 +40,30 @@ const Header = () => {
         <img src={logo} alt="logo" className="h-8 mx-2" />
       </div>
       <div className="col-span-10 px-10 ">
-        <input type="text" className="border border-gray-500 w-1/2 p-2 rounded-l-full" />
-        <button className="p-2 border border-gray-500 rounded-r-full bg-gray-100 py-2 px-5">
-          Search
-        </button>
+        <div>
+          <input
+            type="text"
+            className="border border-gray-500 w-1/2 p-2 rounded-l-full"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onFocus={() => setShowSuggestion(true)}
+            onBlur={() => setShowSuggestion(false)}
+          />
+          <button className="p-2 border border-gray-500 rounded-r-full bg-gray-100 py-2 px-5">
+            Search
+          </button>
+        </div>
+        {showSuggestion && (
+          <div className="bg-white mt-1 px-3 py-2 w-[33rem] border border-gray-100 rounded-xl shadow-md absolute">
+            {searchSuggestion.map((sugg) => {
+              return (
+                <li className="py-2 px-3 shadow-sm hover:bg-gray-100" key={sugg}>
+                  {sugg}
+                </li>
+              )
+            })}
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img src={user} alt="user" className="h-8" />
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const VideoCard = ({ info }) => {
   return (
-    <div className="p-2 m-4 w-56 shadow-lg">
+    <div className="p-2 m-4 w-56 shadow-lg h-[20rem]">
       <img className="rounded-lg" alt="thumbnail" src={info?.snippet?.thumbnails?.medium.url} />
       <ul>
-        <li className="font-bold py-2">{info?.snippet?.title}</li>
+        <li className="font-bold py-2">{`${info?.snippet?.title?.substring(0, 30)}........`}</li>
         <li>{info?.snippet?.channelTitle}</li>
         <li>{info?.statistics?.viewCount} views</li>
       </ul>
